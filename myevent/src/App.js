80536618e5f09da49d612a3537a581f03fe95253
akhenatix <souleymane.epitech.eu>
2022-07-04 16:07:41 +0200

    {[ADD] components: navbar, hearder, react rooter dom, home}

--- myevent/src/App.js
+++ myevent/src/App.js
@@ -1,7 +1,18 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import "./App.css"
+import EventFiche from './components/EventFiche'
+import Header from './components/Header'
+import Home from './components/Home'
 
 export default function App() {
   return (
-    <div>App</div>
+    <div className='App'>
+        <Header />
+        <Routes>
+            <Route path="/" element={<Home/>} />
+            <Route path="/event" element={<EventFiche/>} />
+        </Routes>
+    </div>
   )
 }
---
+++ myevent/src/components/Header.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function Header() {
+  return (
+    <div className='header'>
+     <div className='navbar'>
+    <nav className='navlink'>
+        <div className='main-logo'>
+        {/* <img src={logo} alt='logo' className='logo' /> */}
+        <Link to="/" className='link'>Home</Link>
+        <Link to="/event" className='link'>Event</Link>
+        </div>
+        <form className='navform'>
+          <input  type="search" placeholder="search series"/>
+          <input  type="submit" value="recherche"/>
+        </form>
+    </nav>
+
+    </div>
+        
+    </div>
+  )
+}
